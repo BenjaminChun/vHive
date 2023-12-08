b160556e12dff89a81bbc9cb80959070a1b8fcf5
BenChun <67034610+BenjaminChun@users.noreply.github.com>
2023-12-08 12:04:40 +0800

    fix: added chmod

--- ben/ssh.sh
+++ ben/ssh.sh
@@ -1,4 +1,5 @@
 cd ./firecracker-containerd
 git checkout firecracker-v1.4.1-vhive-integration-debug
 cd ./tools/image-builder
+chmod 600 ./firecracker_rsa
 ssh -vvv -i ./firecracker_rsa root@172.16.0.1
