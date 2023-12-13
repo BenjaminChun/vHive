f117a85935743201a55d811068cf907aac586960
Fong Jing Han <fongjinghan@Fongs-MacBook-Air.local>
2023-12-13 12:21:46 +0800

    fix: added line to switch branch

--- ben/clone.sh
+++ ben/clone.sh
@@ -3,6 +3,7 @@
 # Get the repository URLs from the command line arguments
 repo_url1="https://github.com/BenjaminChun/vHive.git"
 repo_url2="https://github.com/vhive-serverless/firecracker-containerd.git"
+branch_name="ssh-test"
 
 # Clone the first repository
 git clone "$repo_url1"
@@ -12,6 +13,17 @@ if [ $? -eq 0 ]; then
     echo "First repository cloned successfully. Check the directory."
 else
     echo "Error: Failed to clone the first repository."
+    exit 1
+fi
+
+# Switch to ssh-test branch
+cd ./vHive && git checkout "$branch_name" && cd ~
+
+# Check if the checkout branch was successful
+if [ $? -eq 0 ]; then
+    echo "Switched to $branch_name branch"
+else
+    echo "Error in switching branch"
     exit 1
 fi
 
